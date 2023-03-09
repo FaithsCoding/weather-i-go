625e96561a9ea5844a922465b55a5bfbf9ea3d03
dExIT <dexit@dyc.lv>
2023-03-09 20:48:20 +0000

    ADded error log fucntion

---
+++ assets/js/errorhandler.js
@@ -0,0 +1,7 @@
+// basic error handling code
+
+function errorHandler(error){
+    console.error(error); // log the error message to the console
+    // modalFactory("There was an error: " + error.message); // display the error message
+
+}
