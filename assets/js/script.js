fc5387b7b21e56347950ebd3ad7f625e5cce9f3b
dExIT <dexit@dyc.lv>
2023-03-11 22:07:56 +0000

    Updated form and style

--- assets/js/script.js
+++ assets/js/script.js
@@ -28,4 +28,26 @@ $('#my-location').on('click', function(event){
     }
 });
 
+var optionsCalendar = '';
+// do the calendar
+// Initialize all input of type date
+var calendars = bulmaCalendar.attach('[type="datetime-local"]', optionsCalendar);
+
+// Loop on each calendar initialized
+for(var i = 0; i < calendars.length; i++) {
+	// Add listener to select event
+	calendars[i].on('select', date => {
+		console.log(date);
+	});
+}
+
+// To access to bulmaCalendar instance of an element
+var element = document.querySelector('#dateTime ');
+if (element) {
+	// bulmaCalendar instance is available as element.bulmaCalendar
+	element.bulmaCalendar.on('select', function(datepicker) {
+		console.log(datepicker.data.value());
+	});
+}
+
 });
