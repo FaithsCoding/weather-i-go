ecf030274bc7a2426610c0ac4898e5ccfd8e0fd9
dExIT <dexit@dyc.lv>
2023-03-11 18:43:01 +0000

    Updated the base for function

--- assets/js/modals.js
+++ assets/js/modals.js
@@ -1,4 +1,5 @@
-/*<div class="modal">
+/*
+<div class="modal">
   <div class="modal-background"></div>
   <div class="modal-card">
     <header class="modal-card-head">
@@ -13,11 +14,31 @@
       <button class="button">Cancel</button>
     </footer>
   </div>
-</div>*/
+</div>
+*/
 
 // Modal
 
-function modaFactory( modalSelector ) {
-
-
+function modalFactory( modalID, modalTitle, modalContent, isActive = false ) {
+  
+  var modalHTML = '' +
+    ' <div id="modal-' + modalID + '" class="modal">' +
+    '   <div class="modal-background"></div>' +
+    '   <div class="modal-card">' +
+      '  <div class="modal-content">' +
+      '  <header class="modal-card-head">' +
+            '<p class="title is-4">'+ modalTitle +'!</p>' +
+            '<p>' + modalContent + ' </p> ' +
+        '   </div>' +
+      '</div>'+
+    '</div>' +
+    '<button class="modal-close is-large" aria-label="close"></button>' +
+  '</div>'  ;
+  // comment
+  // add it to the body
+    jQuery('body').append(modalHTML);
+    jQuery('.modal-close').on('click', function() {
+      $('.modal').remove();
+    });
+  return modalHTML;
 }
