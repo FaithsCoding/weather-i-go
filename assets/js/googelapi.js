2ddf4c632d6dbdf2f5e3ba7c06b4f9654cda2978
dExIT <dexit@dyc.lv>
2023-03-12 00:38:16 +0000

    added zoom level

--- assets/js/googelapi.js
+++ assets/js/googelapi.js
@@ -21,7 +21,7 @@ function initMap() {
 
         map = new google.maps.Map(document.getElementById("map"), {
             center: { lat: position.coords.latitude, lng: position.coords.longitude },
-            zoom: 8,
+            zoom: 14,
           });
       });
     } else {
