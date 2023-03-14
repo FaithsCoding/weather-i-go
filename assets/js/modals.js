916ccbd5a11bd2ea5b15569b9d6f7c6d403eb9fa
dExIT <dexit@dyc.lv>
2023-03-14 19:30:30 +0000

    Udpated modal

--- assets/js/modals.js
+++ assets/js/modals.js
@@ -1,30 +1,11 @@
-/*
-<div class="modal">
-  <div class="modal-background"></div>
-  <div class="modal-card">
-    <header class="modal-card-head">
-      <p class="modal-card-title">Modal title</p>
-      <button class="delete" aria-label="close"></button>
-    </header>
-    <section class="modal-card-body">
-      <!-- Content ... -->
-    </section>
-    <footer class="modal-card-foot">
-      <button class="button is-success">Save changes</button>
-      <button class="button">Cancel</button>
-    </footer>
-  </div>
-</div>
-*/
-
 // Modal
 
 function modalFactory( modalID, modalTitle, modalContent, isActive = false ) {
   try{
   
-  var modal = document.getElementById(modalID);
-  var modalBackground = modal.querySelector('.modal-background');
-  var modalCard = modal.querySelector('.modal-card');
+  //var modal = document.getElementById(modalID);
+  //var modalBackground = modal.querySelector('.modal-background');
+  //var modalCard = modal.querySelector('.modal-card');
   var modalHTML = '' +
     ' <div id="modal-' + modalID + '" class="modal">' +
     '   <div class="modal-background"></div>' +
@@ -48,16 +29,15 @@ function modalFactory( modalID, modalTitle, modalContent, isActive = false ) {
       // silence is golden
     }
     // add listener to close button to any modal modal
-    jQuery('.modal-close').on('click', function() {
+    //jQuery('.modal-close').on('click', function() {
     //  jQuery(this).parent('.modal').removeClass('is-active');
     //});
 
-        jQuery('#modal-' + modalID + ' .modal-close').on('click', function() {
-          jQuery(this).parent('.modal').removeClass('is-active');
-        });
-      return modalHTML;
+    jQuery('#modal-' + modalID + ' .modal-close').on('click', function() {
+      jQuery(this).parent('.modal').removeClass('is-active');
     });
-  }catch(e){
+  return modalHTML;
+}catch(e){
   errorHandler(e);
   console.error(e);
  }
