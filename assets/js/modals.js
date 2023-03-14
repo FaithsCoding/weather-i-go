d69b292cd2bcebd8da58d96a2c0a2e254bfa51c4
Faith Meades <122907573+FaithsCoding@users.noreply.github.com>
2023-03-14 14:34:39 +0000

    Added JS to make map & cards appear when Seach is clicked

--- assets/js/modals.js
+++ assets/js/modals.js
@@ -19,104 +19,45 @@
 
 // Modal
 
-//function modalFactory( modalID, modalTitle, modalContent, isActive = false ) {
- // try{
+function modalFactory( modalID, modalTitle, modalContent, isActive = false ) {
+  try{
   
- // var modal = document.getElementById(modalID);
- // var modalBackground = modal.querySelector('.modal-background');
- // var modalCard = modal.querySelector('.modal-card');
- // var modalHTML = '' +
-  //  ' <div id="modal-' + modalID + '" class="modal">' +
-  //  '   <div class="modal-background"></div>' +
-   // '   <div class="modal-card">' +
-  //    '  <div class="modal-content">' +
-  //    '  <header class="modal-card-head">' +
-   //         '<p class="title is-4">'+ modalTitle +'!</p>' +
-    //        '<p>' + modalContent + ' </p> ' +
-    //    ' </header>' +
-   //   '</div>'+
-    //'</div>' +
-   // '<button class="modal-close is-large" aria-label="close"></button>' +
-  //'</div>'  ;
+  var modal = document.getElementById(modalID);
+  var modalBackground = modal.querySelector('.modal-background');
+  var modalCard = modal.querySelector('.modal-card');
+  var modalHTML = '' +
+    ' <div id="modal-' + modalID + '" class="modal">' +
+    '   <div class="modal-background"></div>' +
+    '   <div class="modal-card">' +
+      '  <div class="modal-content">' +
+      '  <header class="modal-card-head">' +
+            '<p class="title is-4">'+ modalTitle +'!</p>' +
+            '<p>' + modalContent + ' </p> ' +
+        ' </header>' +
+      '</div>'+
+    '</div>' +
+    '<button class="modal-close is-large" aria-label="close"></button>' +
+  '</div>'  ;
   // comment
   // add it to the body
-   // jQuery('body').append(modalHTML);
-    // add the is-active class to the modal if it is true
-   // if ( isActive ) {
-  //    jQuery('#modal-' + modalID).addClass('is-active');
-  //  } else {
-      // silence is golden
-   // }
-    // add listener to close button to any modal modal
- //   jQuery('.modal-close').on('click', function() {
-   // jQuery(this).parent('.modal').removeClass('is-active');
-   // });
-
-   // jQuery('#modal-' + modalID + ' .modal-close').on('click', function() {
-   //   jQuery(this).parent('.modal').removeClass('is-active');
-   // });
-  //return modalHTML;
-//}catch(e){
- // errorHandler(e);
- // console.error(e);
-// }
-//}
-
-/*
-<div class="modal">
-  <div class="modal-background"></div>
-  <div class="modal-card">
-    <header class="modal-card-head">
-      <p class="modal-card-title">Modal title</p>
-      <button class="delete" aria-label="close"></button>
-    </header>
-    <section class="modal-card-body">
-      <!-- Content ... -->
-    </section>
-    <footer class="modal-card-foot">
-      <button class="button is-success">Save changes</button>
-      <button class="button">Cancel</button>
-    </footer>
-  </div>
-</div>
-*/
-
-// Modal
-function modalFactory(modalID, modalTitle, modalContent, isActive = false) {
-  try {
-    var modalHTML = '' +
-      '<div id="modal-' + modalID + '" class="modal">' +
-      '  <div class="modal-background"></div>' +
-      '  <div class="modal-card">' +
-      '    <header class="modal-card-head">' +
-      '      <p class="title is-4">' + modalTitle + '!</p>' +
-      '      <button class="delete modal-close" aria-label="close"></button>' +
-      '    </header>' +
-      '    <section class="modal-card-body">' +
-      '      <p>' + modalContent + '</p>' +
-      '    </section>' +
-      '    <footer class="modal-card-foot">' +
-      '      <button class="button is-success">Save changes</button>' +
-      '      <button class="button modal-close">Cancel</button>' +
-      '    </footer>' +
-      '  </div>' +
-      '</div>';
-
-    // Add the modal HTML to the body of the document
     jQuery('body').append(modalHTML);
-
-    // Add the is-active class to the modal if it is true
-    if (isActive) {
+    // add the is-active class to the modal if it is true
+    if ( isActive ) {
       jQuery('#modal-' + modalID).addClass('is-active');
+    } else {
+      // silence is golden
     }
-
-    // Add listener to close button to any modal
+    // add listener to close button to any modal modal
     jQuery('.modal-close').on('click', function() {
-      jQuery(this).closest('.modal').removeClass('is-active');
-    });
+    //  jQuery(this).parent('.modal').removeClass('is-active');
+    //});
 
-    return modalHTML;
-  } catch (e) {
-    console.error(e);
-  }
+    jQuery('#modal-' + modalID + ' .modal-close').on('click', function() {
+      jQuery(this).parent('.modal').removeClass('is-active');
+    });
+  return modalHTML;
+}) catch(e){
+  errorHandler(e);
+  console.error(e);
+ }
 }
